526b88ccb143dc5af438efb5f94b710b4ab434c0
vivekPandeyDev <codewithvivekpandey@gmail.com>
2025-04-11 18:45:27 +0530

    config server changes

--- config-service/src/main/java/org/loop/troop/ConfigServiceApplication.java
+++ config-service/src/main/java/org/loop/troop/ConfigServiceApplication.java
@@ -2,8 +2,9 @@ package org.loop.troop;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+import org.springframework.cloud.config.server.EnableConfigServer;
 @SpringBootApplication
+@EnableConfigServer
 public class ConfigServiceApplication {
 
 	public static void main(String[] args) {
